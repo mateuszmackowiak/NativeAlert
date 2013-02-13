694991b6481e630370557636b56cf827c2cd88b7
Mateusz Mackowiak <m@mateuszmackowiak.art.pl>
2013-02-13 12:19:59 +0100

    moving to https://github.com/mateuszmackowiak/NativeDialogs

--- build/install.sh
+++
@@ -1,7 +0,0 @@
-
-ADT="/Users/mmmackowiak/Documents/androidSDK/platform-tools/adb"
-
-
-
-"$ADT" uninstall air.NativeAlertMobile
-"$ADT" install /Users/mmmackowiak/Documents/git/NativeDialog/example/NativeAlertMobile/NativeAlertMobile.apk
